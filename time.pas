e47ef0938a95f4d11babeabb85d309cd1bd5c172
Игорь Аведисьян <user@mil8a-202b-07.mmcs.sfedu.ru>
2023-09-30 08:05:14 +0300

    Поменял немного

--- time.pas
+++ time.pas
@@ -1,8 +1,8 @@
 ﻿begin
   var n := readinteger('');
   assert(n>0);
-  if (n mod 4=0) and (not(n mod 100 = 0)) and (n mod 400 <> 0) then
-    print('Високосный')
+  if (n mod 4=0) or (n mod 100 = 0) and (n mod 400 <> 0) then
+    print('Високосный, 366 дней')
   else
-    print('Не високосный');
+    print('Не високосный, 365 дней');
 end.
