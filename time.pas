fca33a1bd75c212b10f3580cd1b168b694b865d2
Игорь Аведисьян <user@mil8a-202b-07.mmcs.sfedu.ru>
2023-09-30 07:53:35 +0300

    Initial commit

---
+++ time.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var n := readinteger('');
+  assert(n>0);
+  if (n mod 4=0) and (not(n mod 100 = 0)) and (n mod 400 <> 0) then
+    print('Високосный')
+  else
+    print('Не високосный');
+end.
