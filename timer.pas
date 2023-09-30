63ed298e459f84cd121ea05410391164b9e64b3e
Игорь Аведисьян <user@mil8a-202b-07.mmcs.sfedu.ru>
2023-09-30 08:19:39 +0300

    Новый файл

---
+++ timer.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var ch := readinteger('Введите количество часов: ');
+  var m := readinteger('Введите количество минут: ');
+  var s := readinteger('Введите количество секунд: ');
+  var sum : integer;
+  sum := (60*ch)*(60*m)*s;
+  print('Сумма секунд: ',sum);
+end.
