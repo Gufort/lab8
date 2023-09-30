059d94cc64239f348dd38ac4832fc661b3919350
Игорь Аведисьян <user@mil8a-202b-07.mmcs.sfedu.ru>
2023-09-30 08:17:45 +0300

    Ваше сообщение

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  print('"Привет, мир!"');
+  var n := readinteger('Введите время: ');
+  assert((n>=0) and (n<24));
+  if (n>=4) and (n<=10) then
+    print('Доброе утро, мир!');
+  if (n>=11) and (n<=16) then
+    print('Добрый день, мир!');
+  if (n>=17) and (n<=22) then
+    print('Добрый вечер, мир!')
+  else
+    print('Доброй ночи, мир!');
 end.
